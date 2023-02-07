94e0b224a0e68665b76c06a2d2bfffdf215fa839
jqllxew <787841563@qq.com>
2023-02-07 19:25:29 +0800

    bad_concepts_threshold

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A NSFW checker for [Web UI](https://github.com/AUTOMATIC1111/stable-diffusion-webui). Replaces non-worksafe images with black squares. Install it from UI.
+
+#### You can freely modify the bad_concepts_threshold value to affect nsfw judgment
--- scripts/censor.py
+++ scripts/censor.py
@@ -1,14 +1,73 @@
-import torch
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
-from transformers import AutoFeatureExtractor
-from PIL import Image
+from abc import ABC
 
-from modules import scripts, shared
+import numpy as np
+import torch
+from PIL import Image
+from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker, cosine_distance
+from transformers import AutoFeatureExtractor
+from transformers import CLIPConfig
+from modules import scripts
 
 safety_model_id = "CompVis/stable-diffusion-safety-checker"
 safety_feature_extractor = None
 safety_checker = None
+bad_concepts_threshold = .0
 
+
+class MySafetyChecker(StableDiffusionSafetyChecker, ABC):
+    def __init__(self, config: CLIPConfig):
+        super().__init__(config)
+
+    @torch.no_grad()
+    def forward(self, clip_input, images):
+        pooled_output = self.vision_model(clip_input)[1]  # pooled_output
+        image_embeds = self.visual_projection(pooled_output)
+
+        # we always cast to float32 as this does not cause significant overhead and is compatible with bfloa16
+        special_cos_dist = cosine_distance(image_embeds, self.special_care_embeds).cpu().float().numpy()
+        cos_dist = cosine_distance(image_embeds, self.concept_embeds).cpu().float().numpy()
+
+        result = []
+        batch_size = image_embeds.shape[0]
+        for i in range(batch_size):
+            result_img = {"special_scores": {}, "special_care": [], "concept_scores": {}, "bad_concepts": []}
+
+            # increase this value to create a stronger `nfsw` filter
+            # at the cost of increasing the possibility of filtering benign images
+            adjustment = 0.0
+
+            for concept_idx in range(len(special_cos_dist[0])):
+                concept_cos = special_cos_dist[i][concept_idx]
+                concept_threshold = self.special_care_embeds_weights[concept_idx].item()
+                result_img["special_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
+                if result_img["special_scores"][concept_idx] > 0:
+                    result_img["special_care"].append({concept_idx, result_img["special_scores"][concept_idx]})
+                    adjustment = 0.01
+
+            for concept_idx in range(len(cos_dist[0])):
+                concept_cos = cos_dist[i][concept_idx]
+                concept_threshold = self.concept_embeds_weights[concept_idx].item()
+                result_img["concept_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
+                if result_img["concept_scores"][concept_idx] > bad_concepts_threshold:
+                    result_img["bad_concepts"].append(concept_idx)
+            result.append(result_img)
+            print(result_img["concept_scores"])
+        has_nsfw_concepts = [len(res["bad_concepts"]) > 0 for res in result]
+        # for idx, has_nsfw_concept in enumerate(has_nsfw_concepts):
+        #     if has_nsfw_concept:
+        #         images[idx] = np.zeros(images[idx].shape)  # black image
+
+        if any(has_nsfw_concepts):
+            print(
+                "Potential NSFW content was detected in one or more images. A black image will be returned instead."
+                " Try again with a different prompt and/or seed."
+            )
+        return images, has_nsfw_concepts
+
+    @torch.no_grad()
+    def forward_onnx(self, clip_input: torch.FloatTensor, images: torch.FloatTensor):
+        return super(MySafetyChecker, self).forward_onnx()
+    
 
 def numpy_to_pil(images):
     """
@@ -28,11 +87,11 @@ def check_safety(x_image):
 
     if safety_feature_extractor is None:
         safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
-        safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
+        safety_checker = MySafetyChecker.from_pretrained(safety_model_id)
 
     safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
-
+    x_checked_image, has_nsfw_concept = safety_checker(
+        images=x_image, clip_input=safety_checker_input.pixel_values)
     return x_checked_image, has_nsfw_concept
 
 
@@ -41,7 +100,7 @@ def censor_batch(x):
     x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim_numpy)
     x = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
-    return x
+    return x, has_nsfw_concept
 
 
 class NsfwCheckScript(scripts.Script):
@@ -53,4 +112,7 @@ class NsfwCheckScript(scripts.Script):
 
     def postprocess_batch(self, p, *args, **kwargs):
         images = kwargs['images']
-        images[:] = censor_batch(images)[:]
+        images[:], has_nsfw_concepts = censor_batch(images)[:]
+        print(f"has_nsfw_concept {has_nsfw_concepts}")
+        p.nsfw_res = has_nsfw_concepts
+
